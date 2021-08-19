87d995273d3eda18bd8b72e1a71a559ddb94c7fb
Aleksei Voronov <despawn@gmail.com>
2021-08-19 15:42:07 +0200

    Add pub uses for Column and Expression types

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,6 @@ mod general;
 mod queries;
 mod tools;
 
-pub use general::{Alias, Aliasable};
+pub use general::{Alias, Aliasable, Column, Expression};
 pub use queries::{delete_from, insert_into, select, update, Joinable, Orderable, Select};
 pub use tools::Parameters;
