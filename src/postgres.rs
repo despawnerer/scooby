0c3446a67bf9c90b9553ab49904fb16378137803
Aleksei Voronov <despawn@gmail.com>
2021-08-28 15:44:17 +0200

    Export more internal structures so that they can be referenced in docs

--- src/postgres.rs
+++ src/postgres.rs
@@ -2,6 +2,11 @@ mod general;
 mod queries;
 mod tools;
 
-pub use general::{with, Alias, Aliasable, Column, Expression};
-pub use queries::{delete_from, insert_into, select, update, Joinable, Orderable, Select};
+pub use general::{
+    with, Alias, Aliasable, Column, Expression, WithClause, WithQuery, WithQueryBuilder,
+};
+pub use queries::{
+    delete_from, insert_into, select, update, BareInsertInto, BareUpdate, DeleteFrom, InsertInto,
+    Joinable, Orderable, Select, Update,
+};
 pub use tools::Parameters;
