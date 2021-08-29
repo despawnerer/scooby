80f96de0a81292e779febea83ef3ee96c70e47fc
Aleksei Voronov <despawn@gmail.com>
2021-08-29 16:21:46 +0200

    Add a quick example to the main page of the library

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,42 @@
 //!
 //! See dialect-specific modules for details on supported features and usage.
 //!
-//! # Requirements
-//!
-//! Requires Rust 1.54 or later.
-//!
 //! # Supported dialects
 //!
 //! - [PostgreSQL][postgres]
-
+//!
+//! # Quick example
+//!
+//! ```
+//! use scooby::postgres::{select, Aliasable, Orderable, Joinable};
+//!
+//! // SELECT
+//! //     country.name AS name,
+//! //     COUNT(*) AS count
+//! // FROM
+//! //     Country AS country
+//! //     INNER JOIN City AS city ON city.country_id = country.id
+//! // WHERE
+//! //     city.population > 1000000
+//! // GROUP BY country.name
+//! // ORDER BY count DESC
+//! // LIMIT 10
+//! select(("country.name".as_("name"), "COUNT(*)".as_("count")))
+//!     .from(
+//!         "Country"
+//!             .as_("country")
+//!             .inner_join("City".as_("city"))
+//!             .on("city.country_id = country.id"),
+//!     )
+//!     .where_("city.population > 1000000")
+//!     .group_by("country.name")
+//!     .order_by("count".desc())
+//!     .limit(10)
+//!     .to_string();
+//! ```
+//!
+//! # Requirements
+//!
+//! Requires Rust 1.54 or later.
 pub mod postgres;
 mod tools;
