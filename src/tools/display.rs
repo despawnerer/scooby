f56464082acef1061f950ca205a9be53b8eeaf1a
Aleksei Voronov <despawn@gmail.com>
2021-12-12 13:20:33 +0100

    Remove unnecessary lifetime parameter from joined(), and replace match with if let

--- src/tools/display.rs
+++ src/tools/display.rs
@@ -1,6 +1,6 @@
 use std::fmt::{self, Display, Formatter};
 
-pub fn joined<'a, I, T>(iter: I, sep: &'a str) -> Joined<'a, I::IntoIter, T>
+pub fn joined<I, T>(iter: I, sep: &str) -> Joined<'_, I::IntoIter, T>
 where
     I: IntoIterator<Item = T>,
     I::IntoIter: Clone,
@@ -29,17 +29,43 @@ where
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         let mut iter = self.iter.clone();
 
-        match iter.next() {
-            Some(first) => {
-                first.fmt(f)?;
-                for item in iter {
-                    f.write_str(self.sep)?;
-                    item.fmt(f)?;
-                }
+        if let Some(first) = iter.next() {
+            first.fmt(f)?;
+            for item in iter {
+                f.write_str(self.sep)?;
+                item.fmt(f)?;
             }
-            _ => {}
         }
-
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::iter::{once, empty, successors};
+    use super::*;
+
+    #[test]
+    fn no_items() {
+        let iter = empty::<usize>();
+        assert_eq!(joined(iter, ", ").to_string(), "");
+    }
+
+    #[test]
+    fn one_item() {
+        let iter = once("asdf");
+        assert_eq!(joined(iter, ", ").to_string(), "asdf");
+    }
+
+    #[test]
+    fn two_items() {
+        let iter = successors(Some(0), |x| Some(x + 1)).take(2);
+        assert_eq!(joined(iter, ", ").to_string(), "0, 1");
+    }
+
+    #[test]
+    fn a_few_items() {
+        let iter = successors(Some(0), |x| Some(x + 1)).take(10);
+        assert_eq!(joined(iter, ", ").to_string(), "0, 1, 2, 3, 4, 5, 6, 7, 8, 9");
     }
 }
